6df622c32ec946e34ee434ea5f5fbecdf21c4d9d
haeseong <junhaesung0428@hanmail.net>
2016-07-30 15:15:12 +0900

    first_commit

---
+++ Roler/app/src/main/java/com/buttering/roler/PlanActivity.java
@@ -0,0 +1,13 @@
+package com.buttering.roler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan);
+    }
+}
